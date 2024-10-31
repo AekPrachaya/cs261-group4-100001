5fa604a1bd072958e8d04938413def7a4b443108
= <=>
2024-11-01 00:26:37 +0700

    refactor(auth): move auth handlers to handler/auth.js

---
+++ handler/auth.js
@@ -0,0 +1,61 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.post('/api/login', async (req, res) => {
+
+    if (!process.env.TU_API_KEY) {
+        console.error("Please set the TU_API_KEY environment variable");
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
+
+    const { username, password } = req.body;
+
+    // Validate request body
+    if (!username || !password) {
+        return res.status(400).json({ error: 'Username and password are required' });
+    }
+
+    try {
+        const response = await fetch('https://restapi.tu.ac.th/api/v1/auth/Ad/verify', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Application-Key": process.env.TU_API_KEY
+            },
+            body: JSON.stringify({
+                "UserName": username,
+                "PassWord": password
+            })
+        });
+
+        // Handle API response statuses
+        if (!response.ok) {
+            const errorText = await response.text();
+            console.error(`API Error: ${errorText}`);
+            return res.status(response.status).json({ error: errorText });
+        }
+
+        const result = await response.json();
+        req.session.user = {
+            id: result["username"],
+        };
+        return res.status(200).json(result);
+    } catch (error) {
+        console.error("Fetch error:", error);
+        return res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+// Logout route
+router.get('api/logout', (req, res) => {
+    req.session.destroy(err => {
+        if (err) {
+            return res.status(500).json({ error: 'Error logging out' });
+        }
+        res.json({ message: 'Logged out successfully' });
+    });
+});
+
+
+export default router;
