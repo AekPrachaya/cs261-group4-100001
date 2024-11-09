b6bfb0971449c1657537ded1f5035d0e7dde18cc
= <=>
2024-11-09 13:23:34 +0700

    fix/feat(server): fix cloudinary relate functions and add deleteDocumentsByPublicIDs

--- server/document.js
+++ server/document.js
@@ -1,37 +1,55 @@
 import { v2 as cloudinary } from 'cloudinary';
-
+import { deleteFile, insertFile } from '../server/db.js';
+import streamifier from 'streamifier';
 
 /* upload document
- * @param {string} path
+ * @param {Multer.File} file
  * @param {string} petitionID
  * @returns {string} public_id
  */
-export const uploadDocument = async (path, petitionID) => {
-    try {
-        const result = await cloudinary.uploader.upload(path, {
-            folder: `petitions/${petitionID}`,
-        });
-        return result.public_id;
-    } catch (error) {
-        console.error(error);
-    }
+export const uploadDocument = async (file, petitionID) => {
+    return new Promise((resolve, reject) => {
+        const stream = cloudinary.uploader.upload_stream(
+            {
+                folder: `petitions/${petitionID}`,
+                resource_type: 'raw',  // For non-image files like PDFs
+            },
+            (error, result) => {
+                if (error) {
+                    reject(error);  // Reject if there's an error
+                } else {
+                    resolve(result.public_id);  // Resolve with the public_id
+                }
+            }
+        );
+
+        // Convert file buffer to a stream and pipe it to Cloudinary
+        streamifier.createReadStream(file.buffer).pipe(stream);
+    });
 }
 /* upload documents
- * @param {Array<string>} paths
+ * @param {Array<Multer.File>} files
  * @param {string} petitionID
  * @returns {Array<string>} public_ids
  * */
-export const uploadDocuments = async (paths, petitionID) => {
-    if (!paths || paths.length === 0) {
+export const uploadDocuments = async (fileBuffers, petitionID) => {
+    if (!fileBuffers || fileBuffers.length === 0) {
         return [];
     }
-
     try {
-        const promises = paths.map((path) => uploadDocument(path, petitionID));
-        const results = await Promise.all(promises);
-        return results.filter(res => res).map((res) => res.public_id);
+        // Upload to Cloudinary
+        const promises = fileBuffers.map((file) => uploadDocument(file, petitionID));
+
+        const public_ids = await Promise.all(promises);
+
+        // Insert to DB
+        const insertPromises = public_ids.map((public_id) => insertFile(petitionID, public_id));
+        await Promise.all(insertPromises);
+        return public_ids;
+
     } catch (error) {
-        console.error(error);
+
+        console.error('error', error);
     }
     return [];
 }
@@ -46,20 +64,40 @@ export const getDocumentsByPetitionID = async (petitionID) => {
         return resources;
     } catch (error) {
         console.error(error);
+        return {};
     }
-    return {};
 }
 
 /* delete document by public_id
  * @param {string} public_id
- * @returns {Object} {success: boolean, message: string}
+ * @returns {string} file
  * */
 export const deleteDocumentByPublicID = async (public_id) => {
     try {
         await cloudinary.uploader.destroy(public_id);
-        return { success: true, message: `Document ${public_id} deleted` };
+        return public_id;
     } catch (error) {
         console.error(error);
-        return { success: false, message: error.message };
     }
 }
+
+/* delete documents by public_ids
+ * @param {Array<string>} public_ids
+ * @return {Promise<Array<string>>} public_ids
+ */
+export const deleteDocumentsByPublicIDs = async (public_ids) => {
+    try {
+        // Delete files on Cloudinary
+        const promises = public_ids.map((public_id) => deleteDocumentByPublicID(public_id));
+        const results = await Promise.all(promises);
+        const successResults = results.filter(res => res.success).map((res) => res.public_id); // Return only succes
+        // Delete files on DB
+        const validDocumentPromises = successResults.map((public_id) => deleteFile(public_id));
+        await Promise.all(validDocumentPromises);
+
+    } catch (error) {
+        console.error(error);
+    }
+    return [];
+}
+
