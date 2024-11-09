aec6f49b612ff806398cbff4aab9ca3d41a39bbf
= <=>
2024-11-10 03:52:00 +0700

    feat(middleware): redirect to / if no session

--- middleware.js
+++ middleware.js
@@ -2,6 +2,6 @@ export const isAuthenticated = (req, res, next) => {
     if (req.session.user) {
         return next();
     }
-    return res.status(401).json({ message: 'Unauthorized access' });
+    return res.redirect('/');
 };
 
