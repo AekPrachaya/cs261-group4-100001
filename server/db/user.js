3d285998d8d2841689949cfdafd3ffdccb4d54bc
= <ChanakanThongrong@gmail.com>
2024-11-23 23:26:25 +0700

    feat(server): make server function more robust

--- server/db/user.js
+++ server/db/user.js
@@ -7,20 +7,31 @@ export const createUser = async (username, password, role) => {
 
     if (!['advisor', 'staff', 'instructor', 'dean'].includes(role))
         throw new Error('Invalid role')
+
+    let hashedPassword;
     try {
-        const hashedPassword = await argon2.hash(password, {
+        hashedPassword = await argon2.hash(password, {
             type: argon2.argon2id, // Specify argon2id
         });
-
-        const { rows } = await POOL.query(
-            'INSERT INTO users (username, password, role) VALUES ($1, $2, $3) RETURNING *',
-            [username, hashedPassword, role]
-        )
-        return rows[0];
-
     } catch (error) {
         console.error('Error hashing password', error)
         throw new Error('Error creating user')
+    }
+
+    try {
+        const { rows } = await POOL.query(
+            'INSERT INTO users (username, password, role) VALUES ($1, $2, $3) ON CONFLICT (username) DO NOTHING RETURNING *',
+            [username, hashedPassword, role]
+        );
+
+        if (rows.length === 0) {
+            return { message: 'Username already exists' };
+        }
+
+        return rows[0];  // return the created user object
+    } catch (error) {
+        console.error('Error inserting user:', error.message);
+        throw new Error('Database error: Unable to create user');
     }
 }
 
@@ -30,6 +41,7 @@ export const getUser = async (username, password) => {
 
     const { rows } = await POOL.query('SELECT * FROM users WHERE username = $1', [username])
     const user = rows[0];
+
 
     if (!user) {
         throw new Error('User not found')
@@ -43,12 +55,23 @@ export const getUser = async (username, password) => {
     return user;
 }
 
-export const removeUser = async (username) => {
-    if (!username)
-        throw new Error('Missing required fields')
 
-    const { rows } = await POOL.query('DELETE FROM users WHERE username = $1 RETURNING *', [username])
-    return rows[0];
-}
+export const removeUser = async (username) => {
+    if (!username) {
+        throw new Error('Missing required fields');
+    }
+    try {
+        const { rows } = await POOL.query(
+            'DELETE FROM users WHERE username = $1 RETURNING *',
+            [username]
+        );
+
+        return rows[0] || null;
+    } catch (error) {
+        console.error('Error removing user:', error.message);
+        throw new Error('Database error: Unable to remove user');
+    }
+};
+
 
 
