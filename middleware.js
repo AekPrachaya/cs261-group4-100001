e7963de107206d0503d0962b95cc98ae7877096b
= <ChanakanThongrong@gmail.com>
2024-11-23 19:04:12 +0700

    feat(middleware): add authorizor middleware

--- middleware.js
+++ middleware.js
@@ -5,3 +5,11 @@ export const isAuthenticated = (req, res, next) => {
     return res.redirect('/');
 };
 
+export const isAuthorizer = (req, res, next) => {
+    const roles = ['advisor', 'staff', 'instructor', 'dean'];
+    if (roles.includes(req.session.user.role)) {
+        return next();
+    }
+    return res.status(403).json({ message: 'User is not a petition authorizer' });
+}
+
