73de7a33d8c3d97e563bbc3f54f19e25c0185c41
= <ChanakanThongrong@gmail.com>
2024-11-26 19:07:44 +0700

    chore(format): format

--- handler/approval.js
+++ handler/approval.js
@@ -1,39 +1,39 @@
 import express from "express";
 import {
-	getApprovalByPetitionId,
-	updateApproval,
+    getApprovalByPetitionId,
+    updateApproval,
 } from "../server/db/approval.js";
 
 const router = express.Router();
 
 router.put("/api/approval/:petition_id", async (req, res) => {
-	const { petition_id } = req.params;
-	const { authorizor, status } = req.body;
+    const { petition_id } = req.params;
+    const { role, status } = req.body;
 
-	const authorizers = ["advisor", "staff", "instructor", "dean"];
-	if (!authorizers.includes(authorizor)) {
-		return res.status(400).json({ error: "Invalid authorizor" });
-	}
+    const roles = ["advisor", "staff", "instructor", "dean"];
+    if (!roles.includes(role)) {
+        return res.status(400).json({ error: "Invalid authorizor" });
+    }
 
-	try {
-		const approval = await updateApproval(petition_id, authorizor, status);
-		return res.status(200).json({ data: approval });
-	} catch (error) {
-		console.error(error);
-		return res.status(500).json({ error: "Internal Server Error" });
-	}
+    try {
+        const approval = await updateApproval(petition_id, role, status);
+        return res.status(200).json({ data: approval });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
 });
 
 router.get("/api/approval/:petition_id", async (req, res) => {
-	const { petition_id } = req.params;
+    const { petition_id } = req.params;
 
-	try {
-		const approval = await getApprovalByPetitionId(petition_id);
-		return res.status(200).json({ data: approval });
-	} catch (error) {
-		console.error(error);
-		return res.status(500).json({ error: "Internal Server Error" });
-	}
+    try {
+        const approval = await getApprovalByPetitionId(petition_id);
+        return res.status(200).json({ data: approval });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
 });
 
 export default router;
--- server/db/approval.js
+++ server/db/approval.js
@@ -9,25 +9,25 @@ import { POOL } from "../db.js";
  * @returns {Object} approval
  * */
 export const createApproval = async (petition_id, comment_id) => {
-	const { rows } = await POOL.query(
-		"INSERT INTO approvals (petition_id, comment_id) VALUES ($1, $2) RETURNING *",
-		[petition_id, comment_id],
-	);
-	return rows[0];
+    const { rows } = await POOL.query(
+        "INSERT INTO approvals (petition_id, comment_id) VALUES ($1, $2) RETURNING *",
+        [petition_id, comment_id],
+    );
+    return rows[0];
 };
 
 export const getApprovalByPetitionId = async (petition_id) => {
-	const { rows } = await POOL.query(
-		"SELECT * FROM approvals WHERE petition_id = $1 LIMIT 1",
-		[petition_id],
-	);
-	return rows[0];
+    const { rows } = await POOL.query(
+        "SELECT * FROM approvals WHERE petition_id = $1 LIMIT 1",
+        [petition_id],
+    );
+    return rows[0];
 };
 
 export const updateApproval = async (petition_id, authorizor, status) => {
-	const { rows } = await POOL.query(
-		`UPDATE approvals SET ${authorizor}_status = $1, ${authorizor}_date = NOW() WHERE petition_id = $2 RETURNING *`,
-		[status, petition_id],
-	);
-	return rows[0];
+    const { rows } = await POOL.query(
+        `UPDATE approvals SET ${authorizor}_status = $1, ${authorizor}_date = NOW() WHERE petition_id = $2 RETURNING *`,
+        [status, petition_id],
+    );
+    return rows[0];
 };
