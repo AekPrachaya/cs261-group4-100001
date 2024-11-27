b2db0936e5e40b53e7be4e3d099650adfc503bbe
= <ChanakanThongrong@gmail.com>
2024-11-27 18:42:01 +0700

    fix(auth): fix auth get override by session middleware

--- index.js
+++ index.js
@@ -47,6 +47,8 @@ app.get("/", (_, res) => {
 	res.sendFile(path.join(__dirname, "public", "html", "login.html"));
 });
 
+app.use(authRouter);
+
 app.use(isAuthenticated);
 
 app.get("/petition", (_, res) => {
@@ -74,7 +76,6 @@ app.get("/check", (_, res) => {
 });
 
 // handlers
-app.use(authRouter);
 app.use(fileRouter);
 app.use(petitionRouter);
 app.use(userRouter);
